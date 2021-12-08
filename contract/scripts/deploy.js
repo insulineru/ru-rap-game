170dee284216ba5148565a81d7bd56430c95b091
Insuline <insuline.ru@gmail.com>
2021-12-09 01:12:42 +0300

    deps(contract): почистил скрипт деплоя и добавил рб

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -3,29 +3,11 @@ const main = async () => {
   const images = ["https://pbs.twimg.com/media/EN7qq_bX0AAIok2.jpg", "https://www.songslyrics.ru/wp-content/uploads/2021/07/Mayot-5.jpg", "https://sun9-18.userapi.com/impg/qvXDo4ycsWuJ7SleRsyvM5u8lAsJX1jzETVr7Q/QHuv4sqU85w.jpg?size=1490x1920&quality=96&sign=e63d1ceae4408db948f6119ca18dc22a&type=album"];
   const hpValues = [2000, 2000, 2002];
   const attackValues = [200, 180, 159];
+  const raidBoss = ['MORGENSHTERN', 'https://persono.ru/Upload/images/26%281%29.JPG', 13000, 44];
   const gameContractFactory = await hre.ethers.getContractFactory('RuRapGame');
-  const gameContract = await gameContractFactory.deploy(names, images, hpValues, attackValues);
+  const gameContract = await gameContractFactory.deploy(names, images, hpValues, attackValues, ...raidBoss);
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
-
-  let txn;
-  txn = await gameContract.mintCharacterNFT(0);
-  await txn.wait();
-  console.log("Minted NFT #1");
-
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log("Minted NFT #2");
-
-  txn = await gameContract.mintCharacterNFT(2);
-  await txn.wait();
-  console.log("Minted NFT #3");
-
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log("Minted NFT #4");
-
-  console.log("Done deploying and minting!");
 };
 
 const runMain = async () => {
