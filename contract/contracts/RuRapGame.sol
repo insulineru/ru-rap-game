65278498460271fd38ed1403985f92bb2948aee2
Insuline <insuline.ru@gmail.com>
2021-12-08 23:58:28 +0300

    feat(contract): наследуюсь от ERC721 и записываю холдера NFT

--- contract/contracts/RuRapGame.sol
+++ contract/contracts/RuRapGame.sol
@@ -2,9 +2,13 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 import "hardhat/console.sol";
 
-contract RuRapGame {
+contract RuRapGame is ERC721 {
 
   struct CharacterAttributes {
     uint characterIndex;
@@ -15,7 +19,15 @@ contract RuRapGame {
     uint attackDamage;
   }
 
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
   CharacterAttributes[] defaultCharacters;
+
+  mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+  mapping(address => uint256) public nftHolders;
+
 
 
   constructor(
@@ -23,8 +35,7 @@ contract RuRapGame {
     string[] memory characterImageURIs,
     uint[] memory characterHp,
     uint[] memory characterAttackDmg
-  )
-  {
+  ) ERC721("Rappers", "RAPPER") {
     for(uint i = 0; i < characterNames.length; i += 1) {
       defaultCharacters.push(CharacterAttributes({
         characterIndex: i,
@@ -38,5 +49,28 @@ contract RuRapGame {
       CharacterAttributes memory c = defaultCharacters[i];
       console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
     }
+
+    _tokenIds.increment();
+  }
+
+  function mintCharacterNFT(uint _characterIndex) external {
+    uint256 newItemId = _tokenIds.current();
+
+    _safeMint(msg.sender, newItemId);
+
+    nftHolderAttributes[newItemId] = CharacterAttributes({
+      characterIndex: _characterIndex,
+      name: defaultCharacters[_characterIndex].name,
+      imageURI: defaultCharacters[_characterIndex].imageURI,
+      hp: defaultCharacters[_characterIndex].hp,
+      maxHp: defaultCharacters[_characterIndex].maxHp,
+      attackDamage: defaultCharacters[_characterIndex].attackDamage
+    });
+
+    console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+
+    nftHolders[msg.sender] = newItemId;
+
+    _tokenIds.increment();
   }
 }
