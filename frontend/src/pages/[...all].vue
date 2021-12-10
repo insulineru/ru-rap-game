544a18ba59ca97f81e8667768f6ba32b1b07524a
Insuline <insuline.ru@gmail.com>
2021-12-10 16:29:02 +0300

    feat(frontend): добавил фронтенд темплейт

---
+++ frontend/src/pages/[...all].vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    Not Found.
+    <router-link to="/" class="text-gray-400">Go to index Page</router-link>
+  </div>
+</template>
---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div text-4xl inline-block>👋🏻</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
